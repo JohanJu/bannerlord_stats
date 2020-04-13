c6384c96afa1e8cadb7b7b8f5832e42e750e0428
Johan Ju <johanj93@gmail.com>
2020-04-13 04:50:34 +0200

    Updates

---
+++ precache-manifest.bd8499ae58abc2b0b2c69fc51b8724b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ddb885dd04fc97d538ab234fd13188",
+    "url": "/bannerlord_stats/index.html"
+  },
+  {
+    "revision": "852891b5aab5df24fd1a",
+    "url": "/bannerlord_stats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c004cc68ac4cf69359a6",
+    "url": "/bannerlord_stats/static/js/2.fc8304b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bannerlord_stats/static/js/2.fc8304b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852891b5aab5df24fd1a",
+    "url": "/bannerlord_stats/static/js/main.57ba4b7d.chunk.js"
+  },
+  {
+    "revision": "0449891592becf5a42fd",
+    "url": "/bannerlord_stats/static/js/runtime-main.2a0b4713.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bannerlord_stats/static/media/logo.5d5d9eef.svg"
+  }
+]);
